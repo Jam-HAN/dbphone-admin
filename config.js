f7a525e5467bcfbd87be5d0fa2ad1439f326a2ff
Jam-HAN <m67776444@gmail.com>
2025-09-25 19:57:33 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // 🔧 설정: 여기 한 줄만 바꾸면 됩니다.
 // Apps Script Web App 배포 URL(마지막에 /exec 포함)
 window.DBP_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbyNELlH5wpjTWsff0-J3Po4fRiptM-ALuYpQneqJe3qrWyBHF7_zJ40fWwBIKACsbqa/exec", // ← 여기만 교체
+  API_BASE: "https://script.google.com/macros/s/AKfycby70Fa9E4ZFXBfzthMA6ljZq4Nn6bt_i8kwgPV0o1vasZB07vgkdPJF2HF6fdjLT24xTA/exec", // ← 여기만 교체
   AUTH_KEY: "" // (선택) 쓰기 API 보호용 키를 쓴다면 여기에 입력
 };
